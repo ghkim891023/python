d57092f27e7fb9a1140b28bee863d796798877d9
ghkim891023 <ghkim891023@gmail.com>
2021-01-11 02:26:31 +0900

    부녀회장 맞았습니다.

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
-# 백준 부녀회장
+# 백준 수학1. 부녀회장
+#k = 2
+#n = 3
+T = int(input())
 
-k = 2
-n = 3
-mylist = []
-mylist.append( list(x for x in range(1,n+1)) )
+for t in range(T):
 
-for i in range(1, n):
-    mylist.append(list())
+    k = int(input())
+    n = int(input())
 
-    for x in range(0, n):
-        Bsum = 0
-        for y in range(0, x+1):
-            Bsum += mylist[i-1][y]
-        mylist[i].append(Bsum)
-    #print(str(i+1)+"번째(층) 리스트:")
-    #print(mylist)
+    mylist = []
+    mylist.append( list(x for x in range(1,n+1)) ) #1층은 그냥 저장해놓기 [[1, 2, 3]]
 
-#print("최종")
-print(mylist[k][n-1])
+    for i in range(1, k+1):
+        mylist.append(list())
+
+        for x in range(0, n):
+            Bsum = 0
+            for y in range(0, x+1):
+                Bsum += mylist[i-1][y]
+            mylist[i].append(Bsum)
+        #print(str(i+1)+"번째(층) 리스트:")
+        #print(mylist)
+
+    #print("최종")
+    print(mylist[k][n-1])
+
